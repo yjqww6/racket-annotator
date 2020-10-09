1e3e5499a530bd8f2d5c1925d184b5fdb570a622
yjqww6 <343519265@qq.com>
2020-10-09 12:01:30 +0800

    simplify

--- examples/demo.rkt
+++ examples/demo.rkt
@@ -1,25 +1,19 @@
 #lang racket/base
 (require  "../main.rkt"
-          (for-syntax racket/base syntax/name syntax/parse))
+          (for-syntax racket/base syntax/name syntax/parse
+                      syntax/parse/lib/function-header))
 
 (provide (except-out (all-from-out racket/base) #%module-begin))
-
-(begin-for-syntax
-  (define-syntax-class formals
-    #:attributes ([args 1])
-    (pattern () #:with (args ...) #'())
-    (pattern x:id #:with (args ...) #'(x))
-    (pattern (x:id . r:formals) #:with (args ...) #'(x r.args ...)))
-  )
 
 (define-module-begin module-begin
   #:expr
   (syntax-parser
     [(head:/#%plain-lambda fmls:formals body:Expr ...)
      #:with name (datum->syntax #f (syntax-local-infer-name this-syntax #f))
+     #:with (args ...) #'fmls.params
      #'(head
         fmls
-        (#%plain-app displayln (#%plain-app list 'name (~@ 'fmls.args fmls.args) ...))
+        (#%plain-app displayln (#%plain-app list 'name (~@ 'args args) ...))
         body.out ...)]
     [_ #f]))
 
